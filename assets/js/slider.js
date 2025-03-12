458fa11bb7334cea5812b378bba4fd72d83256db
Orochichrys <emmanuelbissa0000@gmail.com>
2025-03-12 09:08:44 +0000

    Intégration de Swiper pour le slider d'images, ajout de la précharge d'images, et mise à jour des styles CSS

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,28 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const slides = document.querySelector('.slider');
-    const slide = document.querySelectorAll('.slide');
-    const totalSlides = slide.length;
-    
-    let index = 0;
-
-    // DUPLIQUER le premier slide et l'ajouter à la fin
-    const firstClone = slide[0].cloneNode(true);
-    slides.appendChild(firstClone);
-
-    function slideShow() {
-        index++;
-        slides.style.transition = "transform 0.5s ease-in-out";
-        slides.style.transform = `translateX(${-index * 100}%)`;
-
-        // Réinitialisation après la dernière slide pour un effet infini
-        if (index === totalSlides) {
-            setTimeout(() => {
-                slides.style.transition = "none"; // Supprimer l'animation
-                slides.style.transform = "translateX(0%)"; // Retour au début
-                index = 0;
-            }, 500); // Attendre la fin de l'animation avant de reset
-        }
-    }
-
-    setInterval(slideShow, 3000);
-});
+ // Initialisation de Swiper avec les options souhaitées
+ var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000, // Délai de 3 secondes entre chaque slide
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
